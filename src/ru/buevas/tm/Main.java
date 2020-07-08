1a3b40c01b0444be7d4a98cc1cde12716e9a5228
Andrey Buev <bvasv@mail.ru>
2020-07-08 21:04:36 +0300

    Реализовано терминальное приложение с одним аргументом

--- src/ru/buevas/tm/Main.java
+++ src/ru/buevas/tm/Main.java
@@ -3,6 +3,53 @@ package ru.buevas.tm;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
+        printWelcome();
+        run(args);
+    }
+
+    public static void run(final String[] args) {
+        if (args == null || args.length < 1) return;
+
+        final String param = args[0];
+        switch (param) {
+            case "version": {
+                printVersion();
+            }
+            case "about": {
+                printAbout();
+            }
+            case "help": {
+                printHelp();
+            }
+            default: {
+                printError();
+            }
+        }
+    }
+
+    public static void printWelcome() {
+        System.out.println("** WELCOME TO TASK MANAGER **");
+    }
+
+    public static void printVersion() {
+        System.out.println("Task Manager version 1.0.0");
+        System.exit(0);
+    }
+
+    public static void printAbout() {
+        System.out.println("Developer: Andrey Buev (buev_as@mail.ru)");
+        System.exit(0);
+    }
+
+    public static void printHelp() {
+        System.out.println("version - Display version information");
+        System.out.println("about - Display developer information");
+        System.out.println("help - Display list of commands information");
+        System.exit(0);
+    }
+
+    public static void printError() {
+        System.out.println("Error: Unknown parameter");
+        System.exit(-1);
     }
 }
